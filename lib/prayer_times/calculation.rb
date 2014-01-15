0d12f8cda0652c1d03a64a3849e5501b1fdbbd78
Khaled alHabache <khellls@gmail.com>
2014-01-15 07:09:25 +0300

    refactor and format calculation

--- lib/prayer_times/calculation.rb
+++ lib/prayer_times/calculation.rb
@@ -6,7 +6,7 @@ module PrayerTimes
     include MathHelpers
 
     def_delegators :@calculator, :time_suffixes, :time_format, :invalid_time, :iterations_count, :times_offsets
-    
+
     # Gets the prayers times
     # @param [PrayTime] calculator parent class
     # @param [Date] date the desired date
@@ -19,37 +19,37 @@ module PrayerTimes
       self.lng = coords[1]
       self.elv = coords.size > 2 ? coords[2] : 0
       self.time_zone = time_zone
-      date = Date.new(date[0], date[1], date[2]) unless date.is_a? Date
-      self.jdate = date.ajd.to_f - lng / (15 * 24.0)
-    end 
+      self.jdate = julian_date(date) - lng / (15 * 24.0)
+      self.times = {
+        imsak: 5, fajr: 5, sunrise: 6, dhuhr: 12, asr: 13, 
+        sunset: 18, maghrib: 18, isha: 18
+      }
+    end
 
     # Compute prayer times
     # @return [Hash] times
     def compute
-      times = {
-        imsak: 5, fajr: 5, sunrise: 6, dhuhr: 12,
-        asr: 13, sunset: 18, maghrib: 18, isha: 18
-      }
       # main iterations
-      iterations_count.times{times = compute_prayer_times(times)}
-      times = adjust_times(times)
+      iterations_count.times{compute_prayer_times}
 
-      # add midnight time
-      if method_settings[:midnight] == 'Jafari'
-        times[:midnight] = times[:sunset] + time_diff(times[:sunset], times[:fajr]) / 2
-      else
-        times[:midnight] = times[:sunset] + time_diff(times[:sunset], times[:sunrise]) / 2
-      end
+      adjust_times
 
-      times = tune_times(times)
-      modify_formats(times)
-      set_names(times)
-    end 
+      tune_times
 
-    private 
+      modify_formats
 
-    attr_accessor :calculator, :time_zone, :lat, :lng, :elv, :jdate
+      set_names
+    end
 
+    private
+
+    attr_accessor :calculator, :time_zone, :lat, :lng, :elv, :jdate, :times
+
+    # get julian date of date object or array
+    def julian_date(date)
+      date_obj = date.is_a?(Date) ? date : Date.new(date[0], date[1], date[2])
+      date_obj.ajd.to_f
+    end
 
     # convert float time to the given format (see time_formats)
     def get_formatted_time(time)
@@ -66,10 +66,10 @@ module PrayerTimes
     # compute mid-day time
     def mid_day(time)
       eqt = sun_position(jdate + time)[1]
-        fix_hour(12 - eqt)
+      fix_hour(12 - eqt)
     end
-      
-    # compute the time at which sun reaches a specific angle below horizon 
+
+    # compute the time at which sun reaches a specific angle below horizon
     def sun_angle_time(angle, time, direction = nil)
       decl = sun_position(jdate + time)[0]
       noon = mid_day(time)
@@ -81,10 +81,10 @@ module PrayerTimes
     end
 
     # compute asr time
-    def asr_time(factor, time) 
+    def asr_time(factor, time)
       decl = sun_position(jdate + time)[0]
       angle = -darccot(factor + rtan(lat - decl).abs)
-        sun_angle_time(angle, time)
+      sun_angle_time(angle, time)
     end
 
     # compute declination angle of sun and equation of time
@@ -107,42 +107,64 @@ module PrayerTimes
 
 
     # compute prayer times at given julian date
-    def compute_prayer_times(times)
-        day_portion(times)
-        {
-          imsak:   sun_angle_time(method_settings[:imsak].to_f, times[:imsak], 'ccw'),
-          fajr:    sun_angle_time(method_settings[:fajr].to_f, times[:fajr], 'ccw'),
-          sunrise: sun_angle_time(rise_set_angle(elv), times[:sunrise], 'ccw'),
-          dhuhr:   mid_day(times[:dhuhr]),
-          asr:     asr_time(asr_factor(method_settings[:asr]), times[:asr]),
-          sunset:  sun_angle_time(rise_set_angle(elv), times[:sunset]),
-          maghrib: sun_angle_time(method_settings[:maghrib].to_f, times[:maghrib]),
-          isha:    sun_angle_time(method_settings[:isha].to_f, times[:isha]) 
-      }
+    def compute_prayer_times
+      day_portion
+
+      initial_computation
+    end
+
+    # make the primary calculations
+    def initial_computation
+      times.merge!(
+        imsak:   sun_angle_time(method_settings[:imsak].to_f, times[:imsak], 'ccw'),
+        fajr:    sun_angle_time(method_settings[:fajr].to_f, times[:fajr], 'ccw'),
+        sunrise: sun_angle_time(rise_set_angle(elv), times[:sunrise], 'ccw'),
+        dhuhr:   mid_day(times[:dhuhr]),
+        asr:     asr_time(asr_factor(method_settings[:asr]), times[:asr]),
+        sunset:  sun_angle_time(rise_set_angle(elv), times[:sunset]),
+        maghrib: sun_angle_time(method_settings[:maghrib].to_f, times[:maghrib]),
+        isha:    sun_angle_time(method_settings[:isha].to_f, times[:isha])
+      )
     end
 
     # adjust times in a prayer time array
-    def adjust_times(times)
-      tz_adjust = time_zone - lng / 15.0
-      times.keys.each{|k| times[k] += tz_adjust}
+    def adjust_times
+
+      adjust_time_zones
+
       if !method_settings[:high_lats].nil?
-        times = adjust_high_lats(times)
+        times = adjust_high_lats
       end
 
       if minute?(method_settings[:imsak])
         times[:imsak] = times[:fajr] - method_settings[:imsak].to_f / 60.0
       end
+
       # need to ask about 'min' settings
       if minute?(method_settings[:maghrib])
         times[:maghrib] = times[:sunset] + method_settings[:maghrib].to_f / 60.0
       end
+
       if minute?(method_settings[:isha])
         times[:isha] = times[:maghrib] + method_settings[:isha].to_f / 60.0
       end
 
       times[:dhuhr] += method_settings[:dhuhr].to_f / 60.0
 
-      return times
+      # add midnight time
+      if method_settings[:midnight] == 'Jafari'
+        times[:midnight] = times[:sunset] + time_diff(times[:sunset], times[:fajr]) / 2
+      else
+        times[:midnight] = times[:sunset] + time_diff(times[:sunset], times[:sunrise]) / 2
+      end
+
+      times
+    end
+
+    # adjust time zones
+    def adjust_time_zones
+      tz_adjust = time_zone - lng / 15.0
+      times.keys.each{|k| times[k] += tz_adjust}
     end
 
     # get asr shadow factor
@@ -157,29 +179,29 @@ module PrayerTimes
     end
 
     # apply offsets to the times
-    def tune_times(times)
+    def tune_times
       times.each{|k,_| times[k] += (method_offsets[k] + times_offsets[k]) / 60.0}
     end
 
     # convert times to given time format
-    def modify_formats(times)
+    def modify_formats
       times.each{|k,_| times[k] = get_formatted_time(times[k])}
     end
-    
-    def set_names(times)
+
+    def set_names
       res = {}
       calculator.times_names.each{|k,v| res[v] = times[k]}
       res
     end
 
     # adjust times for locations in higher latitudes
-    def adjust_high_lats(times)
+    def adjust_high_lats
       night_time = time_diff(times[:sunset], times[:sunrise]) # sunset to sunrise
       times[:imsak] = adjust_hl_time(times[:imsak], times[:sunrise], method_settings[:imsak].to_f, night_time, 'ccw')
       times[:fajr]  = adjust_hl_time(times[:fajr], times[:sunrise], method_settings[:fajr].to_f, night_time, 'ccw')
       times[:isha]  = adjust_hl_time(times[:isha], times[:sunset], method_settings[:isha].to_f, night_time)
       times[:maghrib] = adjust_hl_time(times[:maghrib], times[:sunset], method_settings[:maghrib].to_f, night_time)
-        times
+      times
     end
 
     # adjust a time for higher latitudes
@@ -198,23 +220,21 @@ module PrayerTimes
       portion = 1/2.0  # midnight
       portion = 1/60.0 * angle if hl_method == 'AngleBased'
       portion = 1/7.0 if hl_method == 'OneSeventh'
-        portion * night
-      end
+      portion * night
+    end
 
     # convert hours to day portions
-    def day_portion(times)
+    def day_portion
       times.each{|k,v| times[k] = times[k] / 24.0}
     end
-    
+
     # compute the difference between two times
     def time_diff(time1, time2); fix_hour(time2 - time1); end
-
 
     # detect if input contains 'min'
     def minute?(arg); arg.to_s.include? "min"; end
 
     def fix_hour(hour); fix(hour, 24.0) ; end
-
 
     def fix_angle(angle); fix(angle, 360.0) ; end
 
@@ -223,11 +243,11 @@ module PrayerTimes
       a = a - mode * (a / mode).floor
       a < 0 ? a + mode : a
     end
-    
+
     def method_settings
       @m_settings ||= calculator.calculation_method.settings
     end
-    
+
     def method_offsets
       @m_offsets ||= calculator.calculation_method.offsets
     end
